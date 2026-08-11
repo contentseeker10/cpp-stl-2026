b3a52097dd99ce890d73564b390b76df4b3b2eb4
contentseeker10 <appelsene.design@gmail.com>
2026-08-11 12:30:53 +0300

    format text w/ format lib

--- Strings_and_Formatting/main.cpp
+++ Strings_and_Formatting/main.cpp
@@ -6,9 +6,12 @@
 */
 
 #include <print>
-#include <string>
+#include <format>
 
-using std::print, std::println;
+#include <string>
+#include <numbers>
+
+using std::print, std::println, std::format;
 using std::string, std::string_view;
 
 //{
@@ -18,6 +21,21 @@ using std::string, std::string_view;
 //
 //	println();
 //}
+
+template <typename T>
+struct Frac
+{
+	T n;
+	T d;
+};
+
+template <typename T>
+struct std::formatter<Frac<T>> : std::formatter<int> {
+	template <typename FormatContext>
+	auto format(const Frac<T>& o, FormatContext& ctx) const {
+		return format_to(ctx.out(), "{}/{}", o.n, o.d);
+	}
+};
 
 int main() {
 	{
@@ -38,6 +56,46 @@ int main() {
 			print("{} ", str_data[i]);
 		}
 
+		println();
+	}
+
+	{
+		println("\n--- Format text with the format library ---\n");
+	
+		string who{ "everyone" };
+		int ival{ 42 };
+		double pi{ std::numbers::pi };
+		auto s1 = format("Hello, {}!", who);
+		auto s2 = format("Integer: {}", ival);
+		auto s3 = format("pi: {}", pi);
+
+		for (auto s : { s1, s2, s3 })
+		{
+			println("{}", s);
+		}
+
+		auto s4 = format("Hello {1} {0}", ival, who);
+		auto s5 = format("Hello {0} {1}", ival, who);
+	
+		println("{}\n{}", s4, s5);
+
+		auto s6 = format("{:.<10}", ival);
+		auto s7 = format("{:.>10}", ival);
+		auto s8 = format("{:.^10}", ival);
+		
+		println("{}\n{}\n{}", s6, s7, s8);
+
+		Frac f{ 5, 3 };
+		println("Frac f {{5, 3}}: {}", f);
+
+		println();
+	}
+
+	{
+		println("\n---  ---\n");
+	
+		
+	
 		println();
 	}
 }
