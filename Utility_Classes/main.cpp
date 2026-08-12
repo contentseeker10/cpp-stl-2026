7e5ff86991501b08e418b0c551db6e8addfe8b20
contentseeker10 <appelsene.design@gmail.com>
2026-08-12 23:32:55 +0300

    init new proj

---
+++ Utility_Classes/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * Volodymyr "contentseeker10" Stepanov
+ * August 2026
+ * C++ STL Cookbook Second Edition
+ * Utility Classes
+*/
+
+#include <print>
+
+using std::print, std::println;
+
+//println("\n---  ---\n");
+//
+//// .
+//
+//println();
+
+int main() {
+	{
+		println("\n---  ---\n");
+
+		// .
+
+		println();
+	}
+}
