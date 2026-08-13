d671445b592a122eecec1ffea6e740a0b61cf04d
contentseeker10 <appelsene.design@gmail.com>
2026-08-13 11:53:42 +0300

    manage optional vals w/ std::optional

--- Utility_Classes/main.cpp
+++ Utility_Classes/main.cpp
@@ -6,8 +6,10 @@
 */
 
 #include <print>
+#include <optional>
 
 using std::print, std::println;
+using std::optional;
 
 //println("\n---  ---\n");
 //
@@ -17,9 +19,28 @@ using std::print, std::println;
 
 int main() {
 	{
-		println("\n---  ---\n");
+		println("\n--- Manage optional values with std::optional ---\n");
 
-		// .
+		optional<int> a{ 42 };
+		println("{}", *a);
+
+		using oint = optional<int>;
+
+		oint b{};
+
+		if (b) println("{}", *b);
+		else println("no value");
+
+		a.reset();
+
+		try
+		{
+			println("{}", a.value());
+		}
+		catch (const std::bad_optional_access& e)
+		{
+			println("b.value(): {}", e.what());
+		}
 
 		println();
 	}
