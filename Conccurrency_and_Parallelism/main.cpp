4db39a7116a5b1f5dc6571efb320c38d0cdecebc
contentseeker10 <appelsene.design@gmail.com>
2026-08-16 12:32:43 +0300

    use std::thread for concurrency

--- Conccurrency_and_Parallelism/main.cpp
+++ Conccurrency_and_Parallelism/main.cpp
@@ -18,37 +18,58 @@ using std::chrono::duration;
 using namespace std::this_thread;
 using namespace std::chrono_literals;
 
-//{
-//	println("\n---  ---\n");
-//	
-//	// .
-//
-//	println();
-//}
+using std::thread;
+
+void sleepms(const unsigned ms) {
+	using std::chrono::milliseconds;
+	sleep_for(milliseconds(ms));
+}
+
+void fthread(const int n) {
+	println("this is t{}", n);
+	for (size_t i {}; i < 5; ++i) {
+		sleepms(100 * n);
+		println("t{}: {}", n, i + 1);
+	}
+	println("finishing t{}", n);
+}
+
+
 
 int main() {
-	{
-		println("\n--- Sleep a process for a specific amount of time ---\n");
-		
-		auto t1 = steady_clock::now();
-		println("sleep for 1.3 seconds");
-		sleep_for(1s + 300ms);
+	//{
+	//	println("\n--- Sleep a process for a specific amount of time ---\n");
+	//	
+	//	auto t1 = steady_clock::now();
+	//	println("sleep for 1.3 seconds");
+	//	sleep_for(1s + 300ms);
+	//	println("sleep for 2 seconds");
+	//	sleep_until(steady_clock::now() + 2s);
+	//	duration<double> dur1 = steady_clock::now() - t1;
+	//	println("total duration: {:.5}s", dur1.count());
+	//
+	//	println();
+	//}
 
-		println("sleep for 2 seconds");
-		sleep_until(steady_clock::now() + 2s);
-
-		duration<double> dur1 = steady_clock::now() - t1;
-		println("total duration: {:.5}s", dur1.count());
 	
-		println();
-	}
-
-	{
-		println("\n---  ---\n");
-		
-		// .
+	println("\n--- Use std::thread for concurrency ---\n");
 	
-		println();
-	}
+	//thread t1(fthread, 1);
+	//thread t2(fthread, 2);
+
+	//t1.join();
+	//t2.join();
+
+	//t1.detach();
+	//t2.detach();
+
+	//println("main() sleep 2 sec");
+	//sleepms(2000);
+
+	std::jthread t1(fthread, 1);
+	std::jthread t2(fthread, 2);
+
+	println("\nend of main()");	
+	println();
 
 }
